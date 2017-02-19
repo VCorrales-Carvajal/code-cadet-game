dfb0fef35c88b893f253f29bf77bc9b53438f249
António Almeida <antonio_m_almeida@>
2017-02-19 18:08:47 +0000

    EventType probabilities and choose method

---
+++ src/org/academiadecodigo/bootcamp/codecadetgame/Tester.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.bootcamp.codecadetgame;
+
+import org.academiadecodigo.bootcamp.codecadetgame.server.gamelogic.enums.EventType;
+
+/**
+ * Created by codecadet on 2/19/17.
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/org/academiadecodigo/bootcamp/codecadetgame/server/gamelogic/enums/EventType.java
+++ src/org/academiadecodigo/bootcamp/codecadetgame/server/gamelogic/enums/EventType.java
@@ -4,12 +4,28 @@ package org.academiadecodigo.bootcamp.codecadetgame.server.gamelogic.enums;
  * Created by codecadet on 2/18/17.
  */
 public enum EventType {
-    COLLECTIVE_CHOOSABLE,
-    COLLECTIVE_NON_CHOOSABLE,
-    PERSONAL_CHOOSABLE,
-    PERSONAL_NON_CHOOSABLE;
+    COLLECTIVE_CHOOSABLE(0.25),
+    COLLECTIVE_NON_CHOOSABLE(0.5),
+    PERSONAL_CHOOSABLE(0.75),
+    PERSONAL_NON_CHOOSABLE(1);
 
+    private double probability;
 
+    EventType(double probability) {
+        this.probability = probability;
+    }
 
+    public double getProbability() {
+        return probability;
+    }
 
+    public EventType choose() {
+        int random = (int) Math.random();
+        for (int i = 0; i < EventType.values().length; i++) {
+            if (random < EventType.values()[i].probability) {
+                return EventType.values()[i];
+            }
+        }
+        return null;
+    }
 }
